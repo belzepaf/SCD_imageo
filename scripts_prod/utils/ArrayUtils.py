dc0e4d3ba5fbfc8d89b9a3017ee8b69481ae859c
Mélanie AUBRY <44865955+belzepaf@users.noreply.github.com>
2022-05-19 09:44:29 +0200

    Delete scripts_prod directory

--- scripts_prod/utils/ArrayUtils.py
+++
@@ -1,5 +0,0 @@
-def try_removing_value_in_array(array, value):
-    try:
-        array.remove(value)
-    except ValueError:
-        pass
--- scripts_prod/utils/StringUtils.py
+++
@@ -1,7 +0,0 @@
-import unicodedata
-
-def remove_special_characters(encodedStr):
-    return unicodedata.normalize('NFKD', encodedStr).encode('ascii', 'ignore').decode()
-
-def equal_ignore_cases(strToCompare, strToMatch):
-    return isinstance(strToCompare, str) and remove_special_characters(strToCompare).lower() == strToMatch
